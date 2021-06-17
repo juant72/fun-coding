ffea3a3bc59df3b7ca3d929c3a2a101710d2c427
Juan Tapia <juant72@gmail.com>
2021-06-16 21:42:28 -0300

    C version

---
+++ Big O Notation/Constant Complexity/CSharp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConstantComplexity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputs = new int[] { 2, 3, 4, 5, 6, 7, 8 };
+
+            display_first_cube(inputs);
+        }
+        static void display_first_cube(int[] items)
+        {
+            int results = (int)Math.Pow(items[0], 3);
+            Console.WriteLine(results);
+        }
+    }
+}
