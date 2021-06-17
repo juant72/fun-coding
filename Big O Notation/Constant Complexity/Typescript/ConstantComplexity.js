c7f07c509af2273c27997bfe89719d51dfdc0723
Juan Tapia <juant72@gmail.com>
2021-06-17 01:45:19 -0300

    TS version

---
+++ Big O Notation/Constant Complexity/Typescript/ConstantComplexity.js
@@ -0,0 +1,7 @@
+function display_fisrt_cube(items) {
+    var result;
+    result = Math.pow(items[0], 3);
+    console.log(result);
+}
+var inputs = [2, 3, 4, 5, 6, 7, 8];
+display_fisrt_cube(inputs);
---
+++ Big O Notation/Constant Complexity/Typescript/ConstantComplexity.ts
@@ -0,0 +1,12 @@
+function display_fisrt_cube(items:number[] ){
+    var result
+    result=Math.pow(items[0],3)
+    console.log(result)
+}
+
+let inputs: number[]=[2,3,4,5,6,7,8];
+display_fisrt_cube(inputs)
+
+
+
+
